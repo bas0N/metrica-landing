21774a5d7b054e4277f53306e5307eb43a3940b9
basON <wch.basinski@gmail.com>
2022-09-20 23:24:46 +0200

    Add basic sign-in sign-up modals

--- components/layout/NavbarComponent.tsx
+++ components/layout/NavbarComponent.tsx
@@ -14,7 +14,8 @@ import {
 } from "@nextui-org/react";
 import { useTheme as useNextTheme } from "next-themes";
 import { useRouter } from "next/router";
-
+import SignIn from "../modals/SignIn";
+import SignUp from "../modals/SignUp";
 function NavbarComponent() {
   const router = useRouter();
 
@@ -78,13 +79,9 @@ function NavbarComponent() {
             checked={isDark}
             onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
           />
-          <Navbar.Link color="inherit" href="#">
-            Login
-          </Navbar.Link>
+          <SignIn />
           <Navbar.Item>
-            <Button auto flat as={Link} color="success" href="#">
-              Sign Up
-            </Button>
+            <SignUp />
           </Navbar.Item>
         </Navbar.Content>
       </Navbar.Content>
---
+++ components/modals/SignIn.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  Row,
+  Button,
+  Card,
+  Checkbox,
+  Input,
+  Modal,
+  Spacer,
+  Text,
+  Link,
+} from "@nextui-org/react";
+import { AiOutlineMail } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
+
+function SignIn() {
+  const [visible, setVisible] = React.useState(false);
+  const handler = () => setVisible(true);
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
+  return (
+    <div>
+      <Button
+        light
+        className="text-white"
+        flat
+        as={Link}
+        auto
+        onClick={handler}
+      >
+        Sign in
+      </Button>
+      <Modal
+        closeButton
+        blur
+        aria-labelledby="modal-title"
+        open={visible}
+        onClose={closeHandler}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Welcome to
+            <Text b size={18}>
+              NextUI
+            </Text>
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Email"
+            contentLeft={<AiOutlineMail fill="currentColor" />}
+          />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Password"
+            contentLeft={<RiLockPasswordLine fill="currentColor" />}
+          />
+          <Row justify="space-between">
+            <Checkbox>
+              <Text size={14}>Remember me</Text>
+            </Checkbox>
+            <Text size={14}>Forgot password?</Text>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onClick={closeHandler}>
+            Close
+          </Button>
+          <Button auto onClick={closeHandler}>
+            Sign in
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default SignIn;
