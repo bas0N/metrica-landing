4ee82d49d8d0d11e47067dc3525766313fb602cb
basON <wch.basinski@gmail.com>
2022-09-21 23:16:14 +0200

    Add basic dashboard layout

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function index() {
-  return <div>dashboard</div>;
+  return (
+    <div className="">
+      <div className="grid grid-cols-5 h-screen">
+        <div className=" bg-cyan-600">navigation</div>
+        <div className="col-span-4 bg-teal-600"> content</div>
+      </div>
+    </div>
+  );
 }
 
 export default index;
