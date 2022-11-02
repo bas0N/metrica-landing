f7dfbb2d588a0c5eb057605e970870ad36716dcb
basON <wch.basinski@gmail.com>
2022-11-02 23:19:37 +0100

    Improve offer page

--- pages/offer/index.tsx
+++ pages/offer/index.tsx
@@ -4,66 +4,87 @@ import { Button, Card, Col, Popover, Row, Text } from "@nextui-org/react";
 function index() {
   return (
     <div className="flex flex-col  ">
-      <h1 className="text-4xl font-bold mt-10">Description</h1>
-      <div className="flex flex-col w-1/2 mt-6 mx-auto gap-8">
-        <Card css={{ w: "100%", h: "400px" }}>
-          <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-            <Col>
-              <Text
-                size={12}
-                weight="bold"
-                transform="uppercase"
-                color="#9E9E9E"
-              >
-                Your day your way
-              </Text>
-              <Text h3 color="white">
-                Your checklist for better sleep
-              </Text>
-            </Col>
-          </Card.Header>
-          <Card.Body css={{ p: 0 }}>
-            <Card.Image
-              src="https://clickup.com/landing/images/main/teams/product.png"
-              objectFit="cover"
-              width="100%"
-              height="100%"
-              alt="Relaxing app background"
-            />
-          </Card.Body>
-          <Card.Footer
-            isBlurred
-            css={{
-              position: "absolute",
-              bgBlur: "#ffffff66",
-              borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
-              bottom: 0,
-              zIndex: 1,
-            }}
-          >
-            <Row>
+      <Text className="text-7xl  mt-10">How does it work?</Text>
+      <div className="flex flex-col  mt-6 mx-auto gap-8">
+        <div className="flex gap-10">
+          <Card css={{ w: "50%", h: "400px" }}>
+            <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
               <Col>
-                <Text color="#000" size={16}>
-                  Get notified.
+                <Text
+                  size={12}
+                  weight="bold"
+                  transform="uppercase"
+                  color="#9E9E9E"
+                >
+                  Your day your way
+                </Text>
+                <Text h3 color="white">
+                  Your checklist for better sleep
                 </Text>
               </Col>
-              <Col>
-                <Row justify="flex-end">
-                  <Button flat auto rounded color="secondary">
-                    <Text
-                      css={{ color: "inherit" }}
-                      size={12}
-                      weight="bold"
-                      transform="uppercase"
-                    >
-                      Notify Me
-                    </Text>
-                  </Button>
-                </Row>
-              </Col>
-            </Row>
-          </Card.Footer>
-        </Card>
+            </Card.Header>
+            <Card.Body css={{ p: 0 }}>
+              <Card.Image
+                src="https://clickup.com/landing/images/main/teams/product.png"
+                objectFit="cover"
+                width="100%"
+                height="100%"
+                alt="Relaxing app background"
+              />
+            </Card.Body>
+            <Card.Footer
+              isBlurred
+              css={{
+                position: "absolute",
+                bgBlur: "#ffffff66",
+                borderTop:
+                  "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                bottom: 0,
+                zIndex: 1,
+              }}
+            >
+              <Row>
+                <Col>
+                  <Text color="#000" size={16}>
+                    Get notified.
+                  </Text>
+                </Col>
+                <Col>
+                  <Row justify="flex-end">
+                    <Button flat auto rounded color="secondary">
+                      <Text
+                        css={{ color: "inherit" }}
+                        size={12}
+                        weight="bold"
+                        transform="uppercase"
+                      >
+                        Notify Me
+                      </Text>
+                    </Button>
+                  </Row>
+                </Col>
+              </Row>
+            </Card.Footer>
+          </Card>
+          <div className="flex flex-col  w-1/2">
+            <Text className="text-5xl font-bold"> Feel the magic</Text>
+            <Text className="mt-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Text>
+            <Text className="mt-2">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.
+            </Text>
+          </div>
+        </div>
+
         <Card css={{ w: "100%", h: "400px" }}>
           <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
             <Col>
