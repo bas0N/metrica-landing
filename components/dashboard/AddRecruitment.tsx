947d8bf8549ca4d62517adca2f8abb40715a0e02
basON <wch.basinski@gmail.com>
2022-11-05 18:39:37 +0100

    Add send-form panel and component

--- components/dashboard/AddRecruitment.tsx
+++ components/dashboard/AddRecruitment.tsx
@@ -1,43 +1,88 @@
-import { Button, Checkbox, Input, Radio, Text } from "@nextui-org/react";
-import React from "react";
+import {
+  Button,
+  Checkbox,
+  Input,
+  Radio,
+  Text,
+  Textarea,
+} from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
 
 function AddApplication() {
   const [checked, setChecked] = React.useState("");
+  const [isMobile, setIsMobile] = useState(false);
+  const Mobile = () => {
+    if (typeof window !== "undefined") {
+      if (window.innerWidth < 1024) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  };
+  useEffect(() => {
+    setIsMobile(Mobile());
+    console.log(Mobile());
+  }, [window.innerWidth]);
 
   return (
-    <div className="py-4 px-10 h-full">
+    <div className="py-4 px-10 h-full max-w-[800px]">
       <div className="flex flex-col justify-around h-full">
-        <Text className="text-3xl font-bold">
-          Please provide recruitment details
-        </Text>
+        <Text className="text-5xl  font-bold">Provide recruitment details</Text>
         <Input
           underlined
-          labelPlaceholder="Id of the recruitment process (for internal use)"
+          labelPlaceholder="Id of the recruitment process "
           color="default"
         />
         <Input
           underlined
-          labelPlaceholder="Name of the recruitment process (for respondent)"
-          color="primary"
+          labelPlaceholder="Name of the position"
+          color="default"
         />
+
         <Input width="186px" label="Questionare ending date" type="date" />
+        <Textarea
+          label="Description"
+          helperText="Please enter the description of the reqriutment process"
+          placeholder="Enter your name"
+        />
         <Radio.Group
           label="Select questionare type"
           value={checked}
           onChange={setChecked}
           defaultValue="1"
-          orientation="horizontal"
+          orientation={isMobile ? "vertical" : "horizontal"}
         >
-          <Radio value="1" description="Description for Option1">
+          <Radio
+            value="1"
+            isSquared
+            color="success"
+            description="React, Angular Vue"
+          >
             Frontend
           </Radio>
-          <Radio value="2" description="Description for Option2">
+          <Radio
+            value="2"
+            isSquared
+            color="success"
+            description="NodeJS, Python, Go"
+          >
             Backend
           </Radio>
-          <Radio value="3" description="Description for Option3">
+          <Radio
+            value="3"
+            isSquared
+            color="success"
+            description="AWS, Azure, Docker"
+          >
             Devops
           </Radio>
-          <Radio value="4" description="Description for Option3">
+          <Radio
+            value="4"
+            isSquared
+            color="success"
+            description="Figma, AdobeXd, Sketch"
+          >
             UX/UI
           </Radio>
         </Radio.Group>
--- pages/dashboard/send-form.tsx
+++ pages/dashboard/send-form.tsx
@@ -2,7 +2,7 @@ import { useUser } from "@auth0/nextjs-auth0";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import DashboardLayout from "../../components/layout/DashboardLayout";
-
+import SendFormToApplicant from "../../components/dashboard/SendForm";
 function SendForm() {
   const { user, error, isLoading } = useUser();
   const router = useRouter();
@@ -14,7 +14,7 @@ function SendForm() {
   if (isLoading) {
     return <div></div>;
   } else if (user) {
-    return <div>sendForm</div>;
+    return <SendFormToApplicant />;
   } else if (!user) {
     router.replace("/");
   }
